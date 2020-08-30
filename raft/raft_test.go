008dd05788b3be0f987cbecc9e4f7789e4aebcc3
liangdong <liangdong@smzdm.com>
2020-08-30 21:34:30 +0800

    'update'

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -13,14 +13,23 @@ func TestRaft(t *testing.T) {
 		go func(idx int) {
 			rf, applyChan, _ := Make(addrs, idx, fmt.Sprintf("./node%d", idx))
 			go func() {
+				cnt := 0
+				value := ""
 				for {
 					msg := <-applyChan
 					fmt.Printf("node%d 提交日志: %v\n", idx, msg)
 					if msg.CommandValid {
-						rf.TakeSnapshot(nil, msg.CommandIndex)
+						value = msg.Command.(string)
+						cnt ++
+						if cnt % 5 == 0 {	// 每5条日志做一次snapshot，仅为演示
+							rf.TakeSnapshot([]byte(value), msg.CommandIndex)
+						}
+					} else {
+						value = string(msg.Snapshot)
 					}
 				}
 			}()
+			// 只有leader的Start有作用
 			for {
 				rf.Start("一条日志")
 				time.Sleep(5 * time.Second)
